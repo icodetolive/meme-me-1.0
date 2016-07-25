bcfd278049ebfa04acdad11c011a4ff5b30554e9
Sugandha Naolekar <sugandha.n87@gmail.com>
2016-07-25 06:12:39 -0700

    Add MemeMe 1.0 iOS app to demonstrate more advanced concepts of UIKIT

---
+++ MemeMe 1.0/Meme.swift
@@ -0,0 +1,18 @@
+//
+//  Meme.swift
+//  MemeMe 1.0
+//
+//  Created by Sugandha Naolekar on 7/10/16.
+//  Copyright © 2016 icode. All rights reserved.
+//
+
+import UIKit
+
+    struct Meme {
+        var topText: String
+        var bottomText: String
+        var image: UIImage
+        var memedImage: UIImage
+    }
+
+
